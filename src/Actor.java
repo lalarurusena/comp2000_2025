92f51f8e237cfa989159a34a37f87bc1bb6a43ae
rai <raissamanahan@gmail.com>
2025-09-30 20:22:12 +1000

    undo..

--- src/Actor.java
+++ src/Actor.java
@@ -2,21 +2,13 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Polygon;
 import java.util.List;
-import java.util.ArrayList;
+
 
 public abstract class Actor {
   Color color;
   Cell loc;
   List<Polygon> display;
 
-  List<Item> requiredItems;
-  List<Item> inventory;
-  
-protected Actor(){}
-  protected Actor(List<Item> requiredItems){
-    this.requiredItems = requiredItems;
-    this.inventory = new ArrayList<>();
-  }
 
 
   public void paint(Graphics g) {
